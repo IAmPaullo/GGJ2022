cf875e57a4562e70379140e8e2fc7079da2c0956
Paullo B Felipe <iampaullo@live.com>
2022-01-30 01:46:19 -0300

    efeitos da camera

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField]  Volume globalVolume;
+
+
+    public void Control()
+    {
+        
+    }
+
+
+}
